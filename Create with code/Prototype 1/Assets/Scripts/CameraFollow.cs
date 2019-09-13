2ac448a862bca4bb19fd35cc3a32bfb1ac1f02a8
Kai <kai.n571@stu.nebo.edu>
2019-09-13 10:01:10 -0600

    Added Create with Code Unity project

---
+++ Create with code/Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,7,-10);
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Match position of player
+        transform.position = player.transform.position + offset;
+    }
+}
